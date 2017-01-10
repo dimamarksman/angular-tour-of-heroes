f2b787d89d7ed0f1a2ae14ad5105b98ca049e5ae
Dmitriy Tkachenko <dimamarksman@gmail.com>
2017-01-10 22:47:50 +0200

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
 
+import { Hero }      from './hero';
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div>
+      <label>id: </label>{{hero.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title: string;
+  hero: Hero;
+
+  constructor() {
+    this.title = 'Tour of Heroes';
+    this.hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
+  }
+}
